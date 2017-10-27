3674055e1704bdaaeedc46bdb01f2139c75a1982
Linda Ho <linda.oanh.ho@gmail.com>
2017-10-27 09:39:05 -0500

    Add solutions from lab 5

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -4,20 +4,34 @@ import java.util.Scanner;
 
 public class ReverseLines {
 
-  public static void main(final String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
+    private static LinkedStack<String> inputStack = new LinkedStack<>();
 
+    public static void main(String[] args) {
 
+        System.out.println("Enter multiple lines. Press enter twice to reverse the lines!");
 
-    final Scanner input = new Scanner(System.in);
-    while (input.hasNextLine()) {
-      final String line = input.nextLine();
+        final Scanner input = new Scanner(System.in);
+        String line;
 
+        while (input.hasNextLine()) {
+            line = input.nextLine();
+            if (line.equals("")) {
+                break;
+            } else {
+                fillStack(line);
+            }
+        }
 
-
+        printReverse(inputStack);
     }
 
+    public static void fillStack(String inputString) {
+        inputStack.push(inputString);
+    }
 
-
-  }
+    public static void printReverse(LinkedStack<String> lineStack) {
+        while (!lineStack.isEmpty()) {
+            System.out.println(lineStack.pop());
+        }
+    }
 }
