c5218202d6820ef59b95fc1741660c334cccedf2
Linda Ho <linda.oanh.ho@gmail.com>
2017-10-30 22:28:13 -0500

    Add final solution for ReverseLines

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 
 public class ReverseLines {
 
-  private static LinkedStack<String> inputStack = new LinkedStack<>();
-
   public static void main(final String[] args) {
     final Scanner input = new Scanner(System.in);
     printReverse(input);
@@ -14,15 +12,15 @@ public class ReverseLines {
 
   private static void printReverse(final Scanner input) {
     // Done recursively read and print successive input lines until EOF, then print them out in
-    String line = input.nextLine();
-    if (line.isEmpty()) {
-      for (String s : inputStack.asList()) {
-        System.out.println(s);
-      }
+
+    // Base Case
+    if (!input.hasNextLine()) {
       return;
     }
-    inputStack.push(line);
-    System.out.println(inputStack.peek());
+
+    String line = input.nextLine();
+    System.out.println(line);
     printReverse(input);
+    System.out.println(line);
   }
 }
